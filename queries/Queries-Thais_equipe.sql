16930eccb1c29746d361557241c84704496ff4c8
thaihc <thaisherculanodecarvalho@gmail.com>
2022-02-09 14:49:08 -0300

    Ajustes no arquivo sql

--- queries/Queries-Thais_equipe.sql
+++ queries/Queries-Thais_equipe.sql
@@ -4,50 +4,50 @@ use `formula1`;
 -- Modelagem das tabelas
 
 CREATE TABLE `constructors` (
-  `constructorId` int PRIMARY KEY AUTO_INCREMENT,
-  `constructorRef` varchar(255),
-  `name` varchar(255),
-  `nationality` varchar(255)
+    `constructorId` INT PRIMARY KEY AUTO_INCREMENT,
+    `constructorRef` VARCHAR(255),
+    `name` VARCHAR(255),
+    `nationality` VARCHAR(255)
 );
 
 CREATE TABLE `constructorStandings` (
-  `constructorStandingsId` int PRIMARY KEY AUTO_INCREMENT,
-  `raceId` int,
-  `constructorId` int,
-  `points` int,
-  `position` int,
-  `positionText` varchar(255),
-  `wins` int
+    `constructorStandingsId` INT PRIMARY KEY AUTO_INCREMENT,
+    `raceId` INT,
+    `constructorId` INT,
+    `points` INT,
+    `position` INT,
+    `positionText` VARCHAR(255),
+    `wins` INT
 );
 
 CREATE TABLE `drivers` (
-  `driverId` int PRIMARY KEY AUTO_INCREMENT,
-  `driverRef` varchar(255),
-  `forename` varchar(255),
-  `surname` varchar(255),
-  `nationality` varchar(255)
+    `driverId` INT PRIMARY KEY AUTO_INCREMENT,
+    `driverRef` VARCHAR(255),
+    `forename` VARCHAR(255),
+    `surname` VARCHAR(255),
+    `nationality` VARCHAR(255)
 );
 
 CREATE TABLE `races` (
-  `raceId` int PRIMARY KEY AUTO_INCREMENT,
-  `year` year,
-  `round` int,
-  `circuitId` int,
-  `name` varchar(255),
-  `date` date,
-  `time` time
+    `raceId` INT PRIMARY KEY AUTO_INCREMENT,
+    `year` YEAR,
+    `round` INT,
+    `circuitId` INT,
+    `name` VARCHAR(255),
+    `date` DATE,
+    `time` TIME
 );
 
 CREATE TABLE `results` (
-  `resultId` int PRIMARY KEY AUTO_INCREMENT,
-  `raceId` int,
-  `driverId` int,
-  `constructorId` int,
-  `number` int,
-  `grid` int,
-  `positionOrder` int,
-  `points` int,
-  `laps` int
+    `resultId` INT PRIMARY KEY AUTO_INCREMENT,
+    `raceId` INT,
+    `driverId` INT,
+    `constructorId` INT,
+    `number` INT,
+    `grid` INT,
+    `positionOrder` INT,
+    `points` INT,
+    `laps` INT
 );
 
 -- Conectando as chaves estrangeiras (FK's)
@@ -61,41 +61,58 @@ alter table `results` add foreign key (`constructorId`) references `constructors
 -- Queries/Filtros
 
 -- Quais as equipes que estiveram mais vezes no pódio?
-select constructors.name, count(constructorstandings.position) as quantidade_vezes_podio, 
-sum(constructorstandings.points) as soma_de_pontos 
-from constructorstandings
-inner join constructors
-on constructorstandings.constructorId = constructors.constructorId
-where position <= 3 
-group by name
-order by quantidade_vezes_podio desc
-limit 5;
+SELECT 
+    constructors.name,
+    COUNT(constructorstandings.position) AS quantidade_vezes_podio,
+    SUM(constructorstandings.points) AS soma_de_pontos
+FROM
+    constructorstandings
+        INNER JOIN
+    constructors ON constructorstandings.constructorId = constructors.constructorId
+WHERE
+    position <= 3
+GROUP BY name
+ORDER BY quantidade_vezes_podio DESC
+LIMIT 5;
 
 -- Quais equipes alcançaram mais vezes a primeira posição?
-select constructors.name, constructors.nationality, count(constructorstandings.position) as qtd_1lugar 
-from constructorstandings
-inner join constructors
-on constructorstandings.constructorId = constructors.constructorId
-where position = 1 
-group by name
-order by qtd_1lugar desc
-limit 5;
+SELECT 
+    constructors.name,
+    constructors.nationality,
+    COUNT(constructorstandings.position) AS qtd_1lugar
+FROM
+    constructorstandings
+        INNER JOIN
+    constructors ON constructorstandings.constructorId = constructors.constructorId
+WHERE
+    position = 1
+GROUP BY name
+ORDER BY qtd_1lugar DESC
+LIMIT 5;
 
 -- Quais os pilotos brasileiros responsáveis pela classificação das equipes que ficaram em 1° lugar?
-select constructors.name as name_constructor, constructors.nationality as nationality_constructor, 
-drivers.forename as name_driver, drivers.surname, drivers.nationality as nationality_driver,
-constructorstandings.position, races.year
-from constructorstandings
-inner join constructors
-on constructorstandings.constructorId = constructors.constructorId
-inner join results
-on constructors.constructorId = results.constructorId
-inner join drivers
-on results.driverId = drivers.driverId
-inner join races
-on results.raceId = races.raceId
-where position = 1 and drivers.nationality = 'Brazilian'
-group by surname, name_constructor
-order by name_constructor, year;
+SELECT 
+    constructors.name AS name_constructor,
+    constructors.nationality AS nationality_constructor,
+    drivers.forename AS name_driver,
+    drivers.surname,
+    drivers.nationality AS nationality_driver,
+    constructorstandings.position,
+    races.year
+FROM
+    constructorstandings
+        INNER JOIN
+    constructors ON constructorstandings.constructorId = constructors.constructorId
+        INNER JOIN
+    results ON constructors.constructorId = results.constructorId
+        INNER JOIN
+    drivers ON results.driverId = drivers.driverId
+        INNER JOIN
+    races ON results.raceId = races.raceId
+WHERE
+    position = 1
+        AND drivers.nationality = 'Brazilian'
+GROUP BY surname , name_constructor
+ORDER BY name_constructor , year;
 
 
